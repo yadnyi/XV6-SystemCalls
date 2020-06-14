9292272db17db0ed60ef8f5ea9cde435613bf254
Yadnyi Deshpande <40000613+yadnyi@users.noreply.github.com>
2020-06-14 17:58:34 +0530

    Added Sysytem calls

---
+++ callno.c
@@ -0,0 +1,9 @@
+#include "types.h"
+#include "user.h"
+#include "date.h"
+
+int
+main(int argc, char *argv[]){
+  printf(1, "Total number of System calls till now are : %d\n", callno());
+  exit();
+}
